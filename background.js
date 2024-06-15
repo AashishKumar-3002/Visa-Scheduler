53f248df27174e3588c8309330956072d3b046e1
AashishKumar-3002 <aashish201810kumar@gmail.com>
2024-06-15 22:33:11 +0530

    setting bot speed and bot mode

--- background.js
+++ background.js
@@ -1,14 +1,50 @@
 let timerInterval;
 let startTime;
 let timerStarted = false; 
+let botMode = false;
+let botSpeed = 30;
+let autoCaptchaFillup = false;
+let botNotification = false;
+
+// Get the chrome storage local
+chrome.storage.local.get(['selectedUserProfile'], function(result) {
+    if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+        alert('Error: ' + chrome.runtime.lastError.message);
+        return;
+    }
+
+    var selectedUserProfile = result.selectedUserProfile;
+    
+    chrome.storage.local.get(selectedUserProfile, function(profileData) {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+            alert('Error: ' + chrome.runtime.lastError.message);
+            return;
+        }
+
+        console.log('Profile data:', profileData);
+        if (profileData && profileData[selectedUserProfile]) {
+            botMode = profileData[selectedUserProfile]['botMode'];
+            botSpeed = profileData[selectedUserProfile]['botSpeed'];
+            autoCaptchaFillup = profileData[selectedUserProfile]['autoCaptchaFillup'];
+            botNotification = profileData[selectedUserProfile]['botnotification'];
+        } else {
+            console.error('No profile data found for the selected user profile');
+            alert('Error: No profile data found for the selected user profile');
+        }
+    });
+});
 
 chrome.runtime.onInstalled.addListener(() => {
     // Create an alarm to check for slots every 10 minutes
-    chrome.alarms.create("checkSlotsAlarm", { delayInMinutes: 30, periodInMinutes: 30 });
+    chrome.alarms.create("checkSlotsAlarm", { delayInMinutes: botSpeed, periodInMinutes: botSpeed });
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-    if (!timerStarted) return;
+    if (!botMode) {
+        if (!timerStarted) return;
+    }
 
     if (alarm.name === "checkSlotsAlarm") {
         fetch("http://localhost:5000/start_process", {
